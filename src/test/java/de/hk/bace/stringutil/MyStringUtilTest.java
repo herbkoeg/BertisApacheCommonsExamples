212db0f49c84e08fe0f9b3e2ae8a888ed2c80503
Herbert Paul Köglsperger <herbkoeg@gmx.de>
2016-09-18 07:52:19 +0200

    Initial Version

---
+++ src/test/java/de/hk/bace/stringutil/MyStringUtilTest.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.hk.bace.stringutil;
+
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author palmherby
+ */
+public class MyStringUtilTest {
+    
+    public MyStringUtilTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @Test
+    public void firstTest(){
+        // StringUtils.
+    }
+    
+}
